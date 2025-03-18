e00220dc5c0faac27fd5f7db820e6bb313bb4a7e
nongrata2 <derbesteindiewelt@mail.ru>
2025-03-19 02:17:24 +0300

    chore: added swagger documentation

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,18 +1,24 @@
 package models
 
-type Song struct {
-	ID          int    `db:"id" json:"id"`
-	Group       string `db:"group_name" json:"group"`
-	Songname    string `db:"song_name" json:"song"`
-	ReleaseDate string `db:"release_date" json:"release_date"`
-	Text        string `db:"text" json:"text"`
-	Link        string `db:"link" json:"link"`
+type SongRequest struct {
+    Group    string `json:"group"`
+    Songname string `json:"song"`
 }
 
+type Song struct {
+    ID          int    `db:"id" json:"id"`
+    Group       string `db:"group_name" json:"group"`
+    Songname    string `db:"song_name" json:"song"`
+    ReleaseDate string `db:"release_date" json:"release_date"`
+    Text        string `db:"text" json:"text"`
+    Link        string `db:"link" json:"link"`
+}
+
+
 type SongFilter struct {
-	Group       string `json:"group_name"`
-	Songname    string `json:"song_name"`
-	ReleaseDate string `json:"release_date"`
-	Text        string `json:"text"`
-	Link        string `json:"link"`
+    Group       string `json:"group_name"`
+    Songname    string `json:"song_name"`
+    ReleaseDate string `json:"release_date"`
+    Text        string `json:"text"`
+    Link        string `json:"link"`
 }
