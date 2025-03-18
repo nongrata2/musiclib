45cb98aa212a354b72cdcc777f20a95a3616ca32
nongrata2 <derbesteindiewelt@mail.ru>
2025-03-18 03:13:17 +0300

    refactor: changed project structure, but still need to deal with migration folder and .env file

--- musiclib/db/migrations/000001_create_songs.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE songs (
-    id SERIAL PRIMARY KEY,
-    group_name TEXT NOT NULL,
-    song_name TEXT NOT NULL,
-    release_date TEXT NOT NULL,
-    text TEXT NOT NULL,
-    link TEXT NOT NULL
-);
